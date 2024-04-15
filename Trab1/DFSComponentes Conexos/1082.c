b4eabf8eae3677477876da2ca21947443be86783
Arthur-Gaudium <93216989+Arthur-Gaudium@users.noreply.github.com>
2024-04-15 19:32:56 -0300

    fix: fix compilation error in gcc execution 1082
    Co-Authored-By: Carlacangussu <40703846+carlacangussu@users.noreply.github.com>

--- Trab1/DFSComponentes Conexos/1082.c
+++ Trab1/DFSComponentes Conexos/1082.c
@@ -3,77 +3,71 @@
 #include <string.h>
 
 #define MAXSIZE 35
-#define true 1
-#define false 0
 
 char no[MAXSIZE];
 char adj[MAXSIZE][MAXSIZE];
-_Bool visit[MAXSIZE];
+int visit[MAXSIZE];
 
 int tam;
 int v, e;
 
-void dfs(int x)
-{
-	visit[x] = true;
+void dfs(int x) {
+	visit[x] = 1;
 	no[tam++] = x;
-	for (int i = 0; i < v; ++i)
-		if (adj[x][i])
-			if (!visit[i])
-				dfs(i);
+	for (int i = 0; i < v; ++i) {
+		if (adj[x][i] && !visit[i]) {
+			dfs(i);
+		}
+	}
 }
 
-int compara(char *a, char *b)
-{
-	if (*a == *b)
-		return 0;
-	else if (*a > *b)
-		return 1;
-	else
-		return -1;	
+int compara(const void *a, const void *b) {
+    char charA = *((char*)a);
+    char charB = *((char*)b);
+
+    if (charA == charB) {
+        return 0;
+    } else if (charA > charB) {
+        return 1;
+    } else {
+        return -1;	
+    }
 }
 
-void main ()
-{
-
+int main () {
 	char a, b;
 	int n;
-    int caso =0;
+    int caso = 0;
 
 	scanf("%d", &n);
 
-	while (n>0)
-	{
+	while (n > 0) {
         n--;
         caso++;
 		scanf("%d %d", &v, &e);
-		
-		for (int i = 0; i < e; ++i)
-		{
+
+		for (int i = 0; i < e; ++i) {
 			scanf(" %c %c", &a, &b);
 			adj[a - 'a'][b - 'a'] = 1;
 			adj[b - 'a'][a - 'a'] = 1;
 		}
-		
+
 		printf("Case #%d:\n", caso);
 		int componentes = 0;
-		for (int i = 0; i < v; ++i)
-		{
-			if (!visit[i])
-			{
+		for (int i = 0; i < v; ++i) {
+			if (!visit[i]) {
 				++componentes, dfs(i);
 				qsort(no, tam, sizeof(char), compara);
 				for (int j = 0; j < tam; ++j){
 					printf("%c,", no[j] + 'a');
                 }
 				printf("\n");
-
 			}
 			tam = 0;
 		}
 		printf("%d connected components\n", componentes);
 		printf("\n");
-		memset(visit, false, sizeof(visit));
+		memset(visit, 0, sizeof(visit));
 		memset(adj, 0, sizeof(adj));
 	}
 }
